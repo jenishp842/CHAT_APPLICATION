2976ba3ee80f99d1f833aee4b6304008f5bcb7fe
badal <badal@solulab.co>
2023-04-12 17:21:41 +0530

    group chat design

--- src/Component/Sidebar.jsx
+++ src/Component/Sidebar.jsx
@@ -1,14 +1,42 @@
 import React from "react";
+import { useState } from "react";
+import GroupModal from "./GroupModal";
+import "./button_ui.css"
 
 const Sidebar = ({ data, setCurrentChat, currentChat, handleSelectUser }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   const onChatHandle = (item) => {
     setCurrentChat(item);
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
   };
   return (
     <>
       <div className="chat__sidebar">
-        <h2>Open Chat</h2>
-
+        <div
+          className="d-flex"
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <div>
+            <h2>Open Chat</h2>
+          </div>
+          <div class="button-group">
+          <div class="hover" onClick={openModal}>
+            <button class="hover">Create Group</button>
+          </div>
+          </div>
+        </div>
         <div>
           <h4 className="chat__header">ACTIVE USERS</h4>
           <div className="chat__users">
@@ -26,7 +54,14 @@ const Sidebar = ({ data, setCurrentChat, currentChat, handleSelectUser }) => {
               ))}
           </div>
         </div>
+        <div>
+          <h4 className="chat__header">ACTIVE GROUPS</h4>
+          <div className="chat__users">
+          
+          </div>
+        </div>
       </div>
+      {isModalOpen && <GroupModal users={data} closeModal={closeModal} />}
     </>
   );
 };
