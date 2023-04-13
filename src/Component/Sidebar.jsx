52cf088356f2c40963746caf4f821154985f8207
badal <badal@solulab.co>
2023-04-13 11:29:01 +0530

    group chat

--- src/Component/Sidebar.jsx
+++ src/Component/Sidebar.jsx
@@ -1,10 +1,12 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import GroupModal from "./GroupModal";
-import "./button_ui.css"
+import "./button_ui.css";
+import { ENDPOINT } from "../Endpoint";
 
 const Sidebar = ({ data, setCurrentChat, currentChat, handleSelectUser }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [activeGroups, setActiveGroups] = useState([]);
 
   const onChatHandle = (item) => {
     setCurrentChat(item);
@@ -17,6 +19,28 @@ const Sidebar = ({ data, setCurrentChat, currentChat, handleSelectUser }) => {
   const closeModal = () => {
     setIsModalOpen(false);
   };
+
+  useEffect(() => {
+    axios
+      .post(
+        `${ENDPOINT}/get-group`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${
+              JSON.parse(localStorage.getItem("token"))?.token
+            }`,
+          },
+        }
+      )
+      .then((response) => {
+        setActiveGroups(response.data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+console.log(activeGroups,"💥")
   return (
     <>
       <div className="chat__sidebar">
@@ -29,35 +53,47 @@ const Sidebar = ({ data, setCurrentChat, currentChat, handleSelectUser }) => {
           }}
         >
           <div>
-            <h2>Open Chat</h2>
+            <h2 />
           </div>
           <div class="button-group">
-          <div class="hover" onClick={openModal}>
-            <button class="hover">Create Group</button>
-          </div>
+            <div class="hover" onClick={openModal}>
+              <button class="hover">Create Group</button>
+            </div>
           </div>
         </div>
-        <div>
-          <h4 className="chat__header">ACTIVE USERS</h4>
-          <div className="chat__users">
-            {data &&
-              data?.map((i) => (
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <div>
+            <h4 className="chat__header">ACTIVE USERS</h4>
+            <div className="chat__users">
+              {data &&
+                data?.map((i) => (
+                  <div
+                    className="user-chat"
+                    onClick={() => {
+                      onChatHandle(i);
+                    }}
+                  >
+                    <img src={i.profilepic} className="chat-image" />
+                    <p className="user_list">{i.name}</p>
+                  </div>
+                ))}
+            </div>
+          </div>
+          <div>
+            <h4 className="chat__header">ACTIVE GROUPS</h4>
+            <div className="chat__users">
+              {activeGroups.map((group) => (
                 <div
                   className="user-chat"
                   onClick={() => {
-                    onChatHandle(i);
+                    setCurrentChat(group);
                   }}
                 >
-                  <img src={i.profilepic} className="chat-image" />
-                  <p className="user_list">{i.name}</p>
+                  <img src={group.image} className="chat-image" />
+                  <p className="user_list">{group.name}</p>
                 </div>
               ))}
-          </div>
-        </div>
-        <div>
-          <h4 className="chat__header">ACTIVE GROUPS</h4>
-          <div className="chat__users">
-          
+            </div>
           </div>
         </div>
       </div>
